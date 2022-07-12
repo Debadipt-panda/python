a6916fc75522ad37d7e18851c1263fcc75102d34
Debadipt-panda <102206291+Debadipt-panda@users.noreply.github.com>
2022-07-12 21:34:19 +0530

    Add files via upload

---
+++ do_while.py
@@ -0,0 +1,5 @@
+i=1
+print(i)
+while i<0:
+    print(i,end=" ")
+    i+=1
---
+++ even_odd.py
@@ -0,0 +1,5 @@
+i=int(input("enter a no to check odd or even"))
+if i%2==0:
+    print("it is s even no")
+else:
+    print("it is odd no")
